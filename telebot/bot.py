d51b7b1edd345c5830610023d7d166c32163d15b
drik-exe <27danik98@gmail.com>
2024-02-03 11:08:56 +0300

    start

---
+++ telebot/bot.py
@@ -0,0 +1,12 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id,
+                     'Привет! Добро пожаловать в бота сбора обратной связи!')
+
