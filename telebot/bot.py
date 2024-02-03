9280bed3eb7657b21d0fffb0343c62dff27bcbbe
drik-exe <27danik98@gmail.com>
2024-02-03 11:47:45 +0300

    handlers.

--- telebot/bot.py
+++ telebot/bot.py
@@ -4,34 +4,36 @@ from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+BANK = ''
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add('Выбрать другой банк', 'Выбрать другую валюту', 'Курс на текущий день')
+    markup.add('Национальный банк', 'Альфа банк', 'Беларусьбанк')
 
-    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Привет, чтобы воспользоваться функционалом "
+                                      "бота сперва выберите банк из меню снизу.", reply_markup=markup)
 
-# Обработчики для каждого действия
-@bot.message_handler(func=lambda message: message.text == 'Выбрать другой банк')
+
+@bot.message_handler(func=lambda message: message.text in ('Национальный банк', 'Альфа банк', 'Беларусьбанк'))
 def choose_bank(message):
-    # Здесь можно добавить логику для выбора другого банка
-    bot.send_message(message.chat.id, "Вы выбрали действие 'Выбрать другой банк'")
+    BANK = message.text
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add('USD', 'EUR', 'GBP', 'JPY')
+    bot.send_message(message.chat.id, f"Ты выбрал {message.text}."
+                                      " Теперь выбери нужную тебе валюту ниже.:", reply_markup=markup)
+
 
 @bot.message_handler(func=lambda message: message.text == 'Выбрать другую валюту')
 def choose_currency(message):
-    # Здесь можно добавить логику для выбора другой валюты
     bot.send_message(message.chat.id, "Вы выбрали действие 'Выбрать другую валюту'")
 
 @bot.message_handler(func=lambda message: message.text == 'Курс на текущий день')
 def current_exchange_rate(message):
-    # Здесь можно добавить логику для получения курса на текущий день
     bot.send_message(message.chat.id, "Вы выбрали действие 'Курс на текущий день'")
 
-# Обработчик для действия "Курс на выбранный день"
 @bot.message_handler(func=lambda message: message.text == 'Курс на выбранный день')
 def custom_exchange_rate(message):
-    # Здесь можно добавить логику для получения курса на выбранный день
     bot.send_message(message.chat.id, "Вы выбрали действие 'Курс на выбранный день'")
 
 
